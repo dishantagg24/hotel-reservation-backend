a6442a9c4acdbf0e5586456346e51463a520ba32
Dishant Aggarwal <dishantaggarwal24@gmail.com>
2023-08-27 23:01:29 +0530

    commit

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use('/api/rooms', roomsRoute);
 
 const PORT = process.env.PORT || 8000;
 
+app.use('/', (req, res) => {
+    res.send("Hello");
+})
+
 app.listen(PORT, () => {
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log('Connected Successfully')).catch((err) => { console.error(err); });
 })
