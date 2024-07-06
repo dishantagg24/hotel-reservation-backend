d7cfeb88a4e4f46785391fa37c75e002c59c1492
Dishant Aggarwal <dishantaggarwal24@gmail.com>
2024-07-06 09:48:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
